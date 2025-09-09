ff1c81705db49155ab1d6d3d0770c96115772a9e
NighTMaReEe53 <yousefswagger1337@gmail.com>
2025-09-09 16:26:22 +0300

    first commit

--- src/Components/Show/Show.tsx
+++ src/Components/Show/Show.tsx
@@ -50,7 +50,7 @@ const Show = ({ name, URL }: IText) => {
         <motion.div
           initial={{ opacity: 0, y: 100 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.2 }}
+          transition={{ duration: 0.5 }}
         >
           {el.poster_path ? (
             <>
